70395ec943a22fcd8d62f6d6b544e39eb0b616f3
Denis Gibikoski <desenvolvimentoweb@amsoft.com.br>
2019-01-28 14:36:00 -0300

    alteracao Denis - tela do emissor

--- src/main/java/com/amsoft/erp/util/date/AmsoftUtilsDate.java
+++ src/main/java/com/amsoft/erp/util/date/AmsoftUtilsDate.java
@@ -3,21 +3,18 @@ package com.amsoft.erp.util.date;
 import java.util.Calendar;
 import java.util.Date;
 
-
 /**
  * 
  * 
- * @author ITAUTEC
+ * @author DENIS
  */
 public class AmsoftUtilsDate {
 
-	
 	/**
-	 * 
-	 * 
+	 * Seta data zero hora Ex: dd/MM/yyyy HH:mm:ss
 	 * 
 	 * @param data
-	 * @return
+	 * @return dd/MM/yyyy 00:00:00
 	 */
 	public static Date getZeroHora(Date data) {
 
@@ -32,13 +29,11 @@ public class AmsoftUtilsDate {
 
 	}
 
-	
 	/**
-	 * 
-	 * 
+	 * Seta data a ultima hora Ex: dd/MM/yyyy HH:mm:ss
 	 * 
 	 * @param data
-	 * @return
+	 * @return dd/MM/yyyy 23:59:59
 	 */
 	public static Date getUltimaHora(Date data) {
 		Calendar cal = Calendar.getInstance();
