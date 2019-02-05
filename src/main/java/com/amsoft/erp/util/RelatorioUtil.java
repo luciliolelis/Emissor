0bc6f14a134d2e0727b11c420bc61eeea518f63b
Denis Gibikoski <desenvolvimentoweb@amsoft.com.br>
2019-02-05 13:34:17 -0300

    Criado Relatorios p/ tela de Logs

--- src/main/java/com/amsoft/erp/util/RelatorioUtil.java
+++ src/main/java/com/amsoft/erp/util/RelatorioUtil.java
@@ -13,54 +13,45 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-
-
 public class RelatorioUtil {
-		
-		
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void imprimeRelatorio(String nomeRelatorio, HashMap parametros, List lista) {
 		try {
-			
+
 			JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(lista);
-			
+
 			FacesContext facesContext = FacesContext.getCurrentInstance();
-			
+
 			facesContext.responseComplete();
-			
-			ServletContext servletContext = 
-					(ServletContext) facesContext.getExternalContext().getContext();
-			
-			  String path = servletContext.getRealPath("/relatorios/");
-			  
-	            parametros.put("SUBREPORT_DIR", path + File.separator);
-	            JasperPrint jasperPrint = 
-	            		JasperFillManager.fillReport(
-	                    servletContext.getRealPath("/relatorios/")
-	                    + nomeRelatorio + ".jasper", parametros, dataSource);
-	            
-	            HttpServletResponse res = 
-	            		(HttpServletResponse) facesContext.getExternalContext().getResponse();
-	            res.setContentType("application/pdf");
-	            
-	            int codigo = (int) (Math.random() * 1000);
-	            
-	            res.setHeader("Content-disposition","inline;filename=relatorio_" + codigo + ".pdf");
-	            byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
-	            
-	            res.getOutputStream().write(b);
-	            
-	            res.getCharacterEncoding();
-	            
-	            facesContext.responseComplete();
-	            
-	        } catch (Exception e){
-	            AmsoftUtils.error("Erro ao imprimir relatório: "+e.toString());
-	            e.printStackTrace();
-	        }
-			
-	
+
+			ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();
+
+			String path = servletContext.getRealPath("/relatorios/");
+
+			parametros.put("SUBREPORT_DIR", path + File.separator);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(
+					servletContext.getRealPath("/relatorios/") + nomeRelatorio + ".jasper", parametros, dataSource);
+
+			HttpServletResponse res = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+			res.setContentType("application/pdf");
+
+			int codigo = (int) (Math.random() * 1000);
+
+			res.setHeader("Content-disposition", "inline;filename=relatorio_" + codigo + ".pdf");
+			byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+
+			res.getOutputStream().write(b);
+
+			res.getCharacterEncoding();
+
+			facesContext.responseComplete();
+
+		} catch (Exception e) {
+			AmsoftUtils.error("Erro ao imprimir relatório: " + e.toString());
+			e.printStackTrace();
+		}
+
 	}
 
-	
 }
